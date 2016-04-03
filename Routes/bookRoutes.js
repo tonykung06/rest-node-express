72fe02cf2714d6fb73f8f0f6db952460fbda798e
tonykung <tonykung06@gmail.com>
2016-04-03 15:01:07 +0800

    rest put and patch

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -33,14 +33,65 @@ var routes = function(Book) {
 		res.status(201).send(book);
 	});
 
-	bookRouter.route('/:bookId').get(function(req, res) {
+	bookRouter.use('/:bookId', function(req, res, next) {
 		Book.findById(req.params.bookId, function(err, book) {
 			if (err) {
 				res.status(500).send(err);
 				return;
 			}
 
-			res.json(book);
+			if (!book) {
+				res.status(404).send("no book found");
+				return;
+			}
+
+			req.book = book;
+			next();
+		});
+	});
+
+	bookRouter.route('/:bookId').get(function(req, res) {
+		res.json(req.book);
+	}).put(function(req, res) {
+		req.book.title = req.body.title;
+		req.book.author = req.body.author;
+		req.book.genre = req.body.genre;
+		req.book.read = req.body.read;
+		req.book.save(function(err) {
+			if (err) {
+				res.status(500).send(err);
+				return;
+			}
+
+			res.json(req.book);
+		});
+	}).patch(function(req, res) {
+		var key;
+
+		if (req.body._id) {
+			delete req.body._id;
+		}
+
+		for (key in req.body) {
+			req.book[key] = req.body[key];
+		}
+
+		req.book.save(function(err) {
+			if (err) {
+				res.status(500).send(err);
+				return;
+			}
+
+			res.json(req.book);
+		});
+	}).delete(function(req, res) {
+		req.book.remove(function(err) {
+			if (err) {
+				res.status(500).send(err);
+				return;
+			}
+
+			res.status(204).send('Book is removed');
 		});
 	});
 
